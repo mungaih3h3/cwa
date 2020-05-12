ca50cbaa78f801bc3732bcc87e06924a4d86337a
unknown <mungaihaha@gmail.com>
2020-05-12 21:45:46 +0300

    Calls and Functions - Final

--- compiler.c
+++ compiler.c
@@ -158,6 +158,7 @@ static int emitJump(uint8_t instruction) {
 }
 
 static void emitReturn() {
+    emitByte(OP_NIL);
   emitByte(OP_RETURN);
 }
 
@@ -677,6 +678,20 @@ static void printStatement() {
   emitByte(OP_PRINT);
 }
 
+static void returnStatement() {
+
+if (current->type == TYPE_SCRIPT) {
+    error("Cannot return from top-level code.");
+  }
+  if (match(TOKEN_SEMICOLON)) {
+    emitReturn();
+  } else {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after return value.");
+    emitByte(OP_RETURN);
+  }
+}
+
 static void whileStatement() {
   int loopStart = currentChunk()->count;
   consume(TOKEN_LEFT_PAREN, "Expect '(' after 'while'.");
@@ -746,6 +761,10 @@ static void statement() {
   else if(match(TOKEN_IF))
   {
       ifStatement();
+  }
+  else if(match(TOKEN_RETURN))
+  {
+      returnStatement();
   }
   else if(match(TOKEN_WHILE))
   {
--- vm.c
+++ vm.c
@@ -6,8 +6,13 @@
 #include "vm.h"
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include <stdarg.h>
 VM vm;
+
+static Value clockNative(int argCount, Value* args) {
+  return NUMBER_VAL((double)clock() / CLOCKS_PER_SEC);
+}
 
 
 static void resetStack() {
@@ -45,12 +50,22 @@ static void runtimeError(const char* format, ...) {
   resetStack();
 }
 
+static void defineNative(const char* name, NativeFn function) {
+  push(OBJ_VAL(copyString(name, (int)strlen(name))));
+  push(OBJ_VAL(newNative(function)));
+  tableSet(&vm.globals, AS_STRING(vm.stack[0]), vm.stack[1]);
+  pop();
+  pop();
+}
+
 void initVM() {
     resetStack();
     vm.objects = NULL;
 
     initTable(&vm.globals);
     initTable(&vm.strings);
+
+    defineNative("clock", clockNative);
 }
 
 
@@ -98,6 +113,14 @@ static bool callValue(Value callee, int argCount) {
     switch (OBJ_TYPE(callee)) {
       case OBJ_FUNCTION:
         return call(AS_FUNCTION(callee), argCount);
+
+    case OBJ_NATIVE: {
+        NativeFn native = AS_NATIVE(callee);
+        Value result = native(argCount, vm.stackTop - argCount);
+        vm.stackTop -= argCount + 1;
+        push(result);
+        return true;
+      }
 
       default:
         // Non-callable object type.
@@ -225,7 +248,19 @@ CallFrame* frame = &vm.frames[vm.frameCount - 1];
         break;
       }
     case OP_RETURN: {
-        return INTERPRET_OK;
+        Value result = pop();
+
+        vm.frameCount--;
+        if (vm.frameCount == 0) {
+          pop();
+          return INTERPRET_OK;
+        }
+
+        vm.stackTop = frame->slots;
+        push(result);
+
+        frame = &vm.frames[vm.frameCount - 1];
+        break;
       }
     case OP_CONSTANT: {
         Value constant = READ_CONSTANT();
