bc1e6ed1492ab66cdefc995d638753b3faa766de
unknown <mungaihaha@gmail.com>
2020-05-13 22:47:20 +0300

    Super Classes - Final - Bugs maybe not sure

--- debug.c
+++ debug.c
@@ -82,6 +82,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
       return constantInstruction("OP_GET_PROPERTY", chunk, offset);
     case OP_SET_PROPERTY:
       return constantInstruction("OP_SET_PROPERTY", chunk, offset);
+    case OP_GET_SUPER:
+      return constantInstruction("OP_GET_SUPER", chunk, offset);
     case OP_EQUAL:
       return simpleInstruction("OP_EQUAL", offset);
     case OP_GREATER:
@@ -109,6 +111,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
       return byteInstruction("OP_CALL", chunk, offset);
     case OP_INVOKE:
       return invokeInstruction("OP_INVOKE", chunk, offset);
+    case OP_SUPER_INVOKE:
+      return invokeInstruction("OP_SUPER_INVOKE", chunk, offset);
     case OP_CLOSURE: {
       offset++;
       uint8_t constant = chunk->code[offset++];
@@ -140,6 +144,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     case OP_FALSE: return simpleInstruction("OP_FALSE", offset);
     case OP_POP:
       return simpleInstruction("OP_POP", offset);
+    case OP_INHERIT:
+      return simpleInstruction("OP_INHERIT", offset);
 
     default:
       printf("Unknown opcode %d\n", instruction);
